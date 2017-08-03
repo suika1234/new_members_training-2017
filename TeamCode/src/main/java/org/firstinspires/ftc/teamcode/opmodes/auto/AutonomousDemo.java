17c1a4cfe58c136a4a019eadab91eab3509defe8
Chloe Lee <main_user@Chloes-MacBook-Air.local>
2017-08-02 17:50:53 -0700

    update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutonomousDemo.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="Test: AutonomousDemo", group="Test")
+public class AutonomousDemo extends LinearOpMode {
+
+    /* Declare OpMode members. */
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Initialize the drive system variables.
+
+        // Wait for the game to start (driver presses PLAY), THIS HAS TO BE HERE!
+        waitForStart();
+
+        //Begin coding what you would like the robot to do during the autonomous period
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TeleopDemo.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Test: TeleopDemo", group="Test")
+
+public class TeleopDemo extends OpMode {
+
+    /* Declare OpMode members. */
+    //Examples:
+    // private DcMotor leftMotor;
+    // private DcMotor rightMotor;
+    // private Servo arm;
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        /* eg: Initialize the hardware variables. Note that the strings used here as parameters
+         * to 'get' must correspond to the names assigned during the robot configuration
+         * step (using the FTC Robot Controller app on the phone).
+         */
+
+        //Example:
+        //drivetrain = new Drivetrain(hardwareMap.dcMotor.get("left"), hardwareMap.dcMotor.get("right"));
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        //Example:
+        //drivetrain.stop();
+
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+        //Example:
+        //drivetrain.drivingmethod(1 * gamepad1.left_stick_y, gamepad1.right_stick_x);
+
+        /* if(gamepad1.x) {
+            drivetrain.stop();
+        } */
+
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+
+    }
+}
